a844a24e851fc67470ec201384ff4bc7e23afc49
fernandotisenai <61765101+fernandotisenai@users.noreply.github.com>
2020-06-23 21:09:38 -0300

    Autor back-end banco

--- src/main/java/br/com/bookapi/models/Autor.java
+++ src/main/java/br/com/bookapi/models/Autor.java
@@ -1,13 +1,26 @@
 package br.com.bookapi.models;
 
+
+import java.io.Serializable;
 import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * @author user
  */
-public class Autor {
+@Entity
+@Table(name = "autor", schema = "public")
+public class Autor implements Serializable{
     
+    @Id
+    @GeneratedValue
     private UUID id;
+    
+    @Column(nullable = false, unique = true)
     private String nome;
 
     public Autor() {
--- src/main/java/br/com/bookapi/resources/AutorResources.java
+++ src/main/java/br/com/bookapi/resources/AutorResources.java
@@ -4,6 +4,10 @@ import br.com.bookapi.models.Autor;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -18,57 +22,49 @@ import javax.ws.rs.core.Response;
 /**
  * @author user
  */
+@Stateless
 @Path("autores")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class AutorResources {
-    
-    static List<Autor> autores = new ArrayList<>();
-    
+
+    @PersistenceContext(unitName = "BookPU")
+    EntityManager entityManager;
+
     @GET
     public List<Autor> getAutores() {
-        return autores;
+        return entityManager
+                .createQuery("SELECT a FROM Autor a", Autor.class)
+                .getResultList();
     }
-    
+
     @POST
     public Response addAutor(Autor autor) {
-        autor.setId(UUID.randomUUID());
-        autores.add(autor);        
-        // return autor;
+        entityManager.persist(autor);
         return Response
                 .status(Response.Status.CREATED)
                 .entity(autor)
                 .build();
     }
-    
+
     @GET
     @Path("{id}")
     public Autor getAutor(@PathParam("id") UUID id) {
-        return findAutor(id);        
+        return entityManager.find(Autor.class, id);
     }
-        
+
     @DELETE
     @Path("{id}")
     public void removeAutor(@PathParam("id") UUID id) {
-        Autor autor = findAutor(id);                 
-        autores.remove(autor);
+        Autor autor = entityManager.find(Autor.class, id);
+        entityManager.remove(autor);
     }
-    
+
     @PUT
     @Path("{id}")
     public Autor updateAutor(@PathParam("id") UUID id, Autor a) {
-        Autor autor = findAutor(id); 
-        autor.setNome(a.getNome());
-        return autor;
+        a.setId(id);
+        entityManager.merge(a);
+        return a;
     }
-        
-    public Autor findAutor(UUID id) {
-        Autor autor = null;   
-        for(Autor a : autores) {
-            if(a.getId().equals(id)) {
-                autor = a;
-            }
-        }
-        return autor;        
-    }
-} 
+}
